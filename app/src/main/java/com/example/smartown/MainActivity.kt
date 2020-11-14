02427cb771f9d4e09bb2e8561f0e203091d41a5b
wilsonpires21 <wilsonpires@ipvc.pt>
2020-11-14 19:06:48 +0000

    .

--- app/src/main/java/com/example/smartown/MainActivity.kt
+++ app/src/main/java/com/example/smartown/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.example.smartown
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.button_irnotas -> {
+                val intent = Intent(this@MainActivity, lista_notas::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
